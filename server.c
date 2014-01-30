513aa1bd2971f6979785bc116bfd66c1672541b4
TitoThePenguin <ajm43@students.uwf.edu>
2014-01-30 14:27:48 -0600

    Update server.c
    Got rid of warnings.

--- server.c
+++ server.c
@@ -13,6 +13,7 @@
 #include <net/if.h>
 #include <signal.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #define MAX_NUM_LISTENER_ALLOWED 5
 #define FALSE 0
@@ -100,7 +101,7 @@ int main()
                 fprintf(stderr, "Could not listen!\n");
                 close(listensockfd);
         }
-                                                                                                                                                                                   102,1-8       33%
+
         //Allows for the IP address of the host server to be retrieved.
         struct ifreq myFreq[20];
         struct ifconf ic;
@@ -115,7 +116,7 @@ int main()
 
         //Prtints the host name, IP address, and port #.
         struct sockaddr_in printSock;
-        int addrLen = sizeof(struct sockaddr);
+        socklen_t addrLen = sizeof(struct sockaddr);
         getsockname(listensockfd, (struct sockaddr *) &printSock, &addrLen);
         fprintf(stderr, "Host name: %s\n", hostname);
         fprintf(stderr, "IP address: %s\n", inet_ntoa(((struct sockaddr_in*)&myFreq[1].ifr_addr)->sin_addr));
@@ -124,7 +125,7 @@ int main()
         //Accepts incoming connections.
         struct sockaddr_in clientAddress;
         int mysockfd;
-        int clientLength = sizeof(clientAddress);
+        socklen_t clientLength = sizeof(clientAddress);
         pthread_t tid[2];
         int threadCount = 0;
         while(1)
@@ -141,19 +142,21 @@ int main()
 void * processRequest(void * mySock)
 {
         int aSock = (intptr_t)mySock;
-        int errCheck, count;
+        int errCheck, count, count2;
         char buffer[256];
         char message[256];
+        char reply[256];
         bzero(buffer,256);
         bzero(message,256);
+        bzero(reply,256);
 
         //Reads from the socket.
         errCheck = read(aSock, buffer, 255);
         if(errCheck < 0)
         {
-fprintf(stderr, "Error reading from socket\n");
+                fprintf(stderr, "Error reading from socket\n");
                 close(aSock);
-                return;
+                return NULL;
         }
         fprintf(stderr, "Here is the message: %s\n", buffer);
 
@@ -173,9 +176,34 @@ fprintf(stderr, "Error reading from socket\n");
                 if(message[count - 7] != ' ' && buffer[count] == '<' && buffer[count + 1] == '/' && buffer[count + 2] == 'e' && buffer[count + 3] == 'c' && buffer[count + 4] == 'h' && buffer[count + 5] == 'o' && buffer[count + 6] == '>' && buffer[count + 7] == '\0')
                 {
                         //Writes the echo response to the socket.
-                        errCheck = write(aSock, "<reply>%s</reply>", message);
+                        reply[0] = '<';
+                        reply[1] = 'r';
+                        reply[2] = 'e';
+                        reply[3] = 'p';
+                        reply[4] = 'l';
+                        reply[5] = 'y';
+                        reply[6] = '>';
+                        count2 = count - 6;
+                        for(count = 0; count < count2; count++)
+                                reply[count + 7] = message[count];
+                        reply[count + 7] = '<';
+                        reply[count + 8] = '/';
+                        reply[count + 9] = 'r';
+                        reply[count + 10] = 'e';
+                        reply[count + 11] = 'p';
+                        reply[count + 12] = 'l';
+                        reply[count + 13] = 'y';
+                        reply[count + 14] = '>';
+                        reply[count + 15] = '\0';
+                        errCheck = write(aSock, reply, sizeof(reply));
+                        if(errCheck < 0)
+                        {
+                                fprintf(stderr, "Error writing to socket\n");
+                                close(aSock);
+                                return NULL;
+                        }
                         close(aSock);
-                        return;
+                        return NULL;
                 }
         }
         //Checks if the message is just <loadavg/> with no extra characters (as loadavg should be).
@@ -186,20 +214,81 @@ fprintf(stderr, "Error reading from socket\n");
                 errCheck = getloadavg(loadavg, 3);
                 if(errCheck > 0)
                 {
-                        errCheck = write(aSock, "<loadavg>%d;%d;%d</loadavg>", loadavg[0], loadavg[1], loadavg[2]);
+                        reply[0] = '<';
+                        reply[1] = 'l';
+                        reply[2] = 'o';
+                        reply[3] = 'a';
+                        reply[4] = 'd';
+                        reply[5] = 'a';
+                        reply[6] = 'v';
+                        reply[7] = 'g';
+                        reply[8] = '>';
+                        memcpy(&reply[9], &loadavg[0], sizeof(loadavg[0]));
+                        reply[13] = ';';
+                        memcpy(&reply[14], &loadavg[1], sizeof(loadavg[1]));
+                        reply[18] = ';';
+                        memcpy(&reply[19], &loadavg[2], sizeof(loadavg[2]));
+                        reply[23] = '<';
+                        reply[24] = '/';
+                        reply[25] = 'l';
+                        reply[26] = 'o';
+                        reply[27] = 'a';
+                        reply[28] = 'd';
+                        reply[29] = 'a';
+                        reply[30] = 'v';
+                        reply[31] = 'g';
+                        reply[32] = '>';
+                        reply[33] = '\0';
+                        errCheck = write(aSock, reply, sizeof(reply));
+                        if(errCheck < 0)
+                        {
+                                fprintf(stderr, "Error writing to socket\n");
+                                close(aSock);
+                                return NULL;
+                        }
                         close(aSock);
-                        return;
+                        return NULL;
                 }
         }
 
         //Writes an error message to the socket if the message was not correctly formatted.
-        errCheck = write(aSock, "<error>unknown format</error>");
+        reply[0] = '<';
+        reply[1] = 'e';
+        reply[2] = 'r';
+        reply[3] = 'r';
+        reply[4] = 'o';
+        reply[5] = 'r';
+        reply[6] = '>';
+        reply[7] = 'u';
+        reply[8] = 'n';
+        reply[9] = 'k';
+        reply[10] = 'n';
+        reply[11] = 'o';
+        reply[12] = 'w';
+        reply[13] = 'n';
+        reply[14] = ' ';
+        reply[15] = 'f';
+        reply[16] = 'o';
+        reply[17] = 'r';
+        reply[18] = 'm';
+        reply[19] = 'a';
+        reply[20] = 't';
+        reply[21] = '<';
+        reply[22] = '/';
+        reply[23] = 'e';
+        reply[24] = 'r';
+        reply[25] = 'r';
+        reply[26] = 'o';
+        reply[27] = 'r';
+        reply[28] = '>';
+        reply[29] = '\0';
+        errCheck = write(aSock, reply, sizeof(reply));
         if(errCheck < 0)
         {
                 fprintf(stderr, "Error writing to socket\n");
                 close(aSock);
-                return;
+                return NULL;
         }
         close(aSock);
-        return;
+        return NULL;
 }
